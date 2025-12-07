024df58ef15dbe1e821916ae17ac96f1bfbcff1a
Thahseen AS <thahseen.sadiqas@gmail.com>
2025-12-07 18:00:52 +0530

    Update API URL for login request

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,8 @@ const Login = ({ setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('/api/users/login', formData);
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/login`, formData)
+
 
       // store token
       localStorage.setItem('token', res.data.token);
